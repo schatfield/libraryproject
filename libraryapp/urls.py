10b3a62e6d11071ef945603506657bf84492b01e
Schatfield <dgoldmine@icloud.com>
2020-02-08 19:46:45 -0600

    added view to list librarians, created new user with admin site, changed SQL Insert statement to match provided tables

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -1,9 +1,11 @@
-from django.contrib import admin
-from django.urls import include, path
 from django.urls import path
-from libraryapp.models import *
+from .views import *
+
+app_name = "libraryapp"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('libraryapp.urls')),
+    path('', book_list, name='home'),
+    path('books/', book_list, name='books'),
+    path('librarians/', list_librarians, name='librarians'),
+
 ]
