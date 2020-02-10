f41ad95666bccb57075ca3bfba711b488d6014f7
Schatfield <dgoldmine@icloud.com>
2020-02-10 14:43:32 -0600

    added virtual env and made changes to url paths during class discussion

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -5,8 +5,12 @@ from .views import *
 app_name = "libraryapp"
 
 urlpatterns = [
+    # path('', home, name='home'),
+    # path('accounts/', include('django.contrib')),
     path('', book_list, name='home'),
     path('books/', book_list, name='books'),
+    # path('books/<int:book_id>/', book_details, name='book'),
+    # path('book/form', book_form, name='book_form'),
     path('librarians/', list_librarians, name='librarians'),
     path('libraries/', list_libraries, name='libraries'),
     path('accounts/', include('django.contrib.auth.urls')),
