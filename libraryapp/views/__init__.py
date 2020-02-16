8364428a000477d6967f5bc71c1952778cb29317
Schatfield <dgoldmine@icloud.com>
2020-02-16 14:54:17 -0600

    added code from chapter 8

--- libraryapp/views/__init__.py
+++ libraryapp/views/__init__.py
@@ -6,5 +6,7 @@ from .books.form import book_form
 from .libraries.form import library_form
 from .books.details import book_details
 from .home import home
+from .books.form import book_form, book_edit_form
+
 
 
